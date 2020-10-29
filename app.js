aa7a39f6bd0b7c1f73e51fe952175d0cef5e40cf
Alexander BORISOV <67188470+syzygyexe@users.noreply.github.com>
2020-10-29 21:04:27 +0900

    Update app.js

--- app.js
+++ app.js
@@ -117,18 +117,86 @@ var fs = require("fs");
 // var http = require("http");
 // var fs = require("fs");
 
-var express = require("express");
-var app = express();
+// var myReadStream = fs.createReadStream(__dirname + "/readme.txt", "utf8");
+// var myWriteStream = fs.createWriteStream(__dirname + "/writeme.txt");
+
+// listen for an event data, and fire chunk whenever we receive any chunk of the data
+// myReadStream.on("data", function (chunk) {
+//   console.log("new chunk received:");
+//   myWriteStream.write(chunk);
+// });
+
+// var http = require("http");
+// var fs = require("fs");
+
+// const server = http.createServer(function (req, res) {
+//   console.log("request was made: " + req.url);
+//   res.writeHead(200, { "Content-Type": "text/html" });
+//   var myReadStream = fs.createReadStream(__dirname + "/index.html", "utf8");
+//   myReadStream.pipe(res);
+// });
+
+// server.listen(3000, "127.0.0.1");
+// console.log("Server is running");
+
+// var http = require("http");
+// var fs = require("fs");
+
+// const server = http.createServer(function (req, res) {
+//   console.log("request was made: " + req.url);
+//   res.writeHead(200, { "Content-Type": "application/json" });
+//   const myObj = {
+//     name: "Ryu",
+//     job: "Ninja",
+//     age: 29,
+//   };
+//   res.end(JSON.stringify(myObj));
+// });
+
+// server.listen(3000, "127.0.0.1");
+// console.log("Server is running");
+
+// var http = require("http");
+// var fs = require("fs");
+
+// const server = http.createServer(function (req, res) {
+//   console.log("request was made: " + req.url);
+//   if (req.url === "/home" || req.url === "/") {
+//     res.writeHead(200, { "Content-Type": "text/html" });
+//     fs.createReadStream(__dirname + "/index.html").pipe(res);
+//   } else if (req.url === "/contact") {
+//     res.writeHead(200, { "Content-Type": "text/html" });
+//     fs.createReadStream(__dirname + "/contact.html").pipe(res);
+//   } else if (req.url === "/api/ninjas") {
+//     var ninjas = [
+//       { name: "ryu", age: 29 },
+//       { name: "yoshi", age: 32 },
+//     ];
+//     res.writeHead(200, { "Content-Type": "application/json" });
+//     res.end(JSON.stringify(ninjas));
+//   } else {
+//     res.writeHead(404, { "Content-Type": "text/html" });
+//     fs.createReadStream(__dirname + "/404.html").pipe(res);
+//   }
+// });
+
+// server.listen(3000, "127.0.0.1");
+// console.log("Server is running");
+
+const express = require("express");
+const app = express();
 
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/index.html");
+  res.send("this is the home page");
 });
 
 app.get("/contact", function (req, res) {
-  res.sendFile(__dirname + "/contact.html");
+  res.send("this is the contact page");
 });
 
-app.get("/profile/name:", function (req, res) {
-  res.send("You are vievwing the profile of " + req.params.name);
+app.get("/profile/:id", function (req, res) {
+  res.send("You requested to see a profile with the id of " + req.params.id);
 });
+
+app.listen(3000);
 
